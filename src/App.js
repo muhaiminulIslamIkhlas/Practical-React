e6b99e71294ea6d84860c514c2e8ca950704b26f
muhaiminulIslamIkhlas <muhaiming2c@gmail.com>
2020-09-19 16:42:44 +0600

    Icon and toastify

--- src/App.js
+++ src/App.js
@@ -1,24 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+
+import "./App.css";
+import { FaAccusoft } from "react-icons/fa";
+import { toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
+const CustomToast = ({ closeToast }) => {
+  return (
+    <div>
+      Something went wrong!v
+      <button onClick={closeToast}>Close</button>
+    </div>
+  );
+};
+
+toast.configure();
 
 function App() {
+  const notify = () => {
+    toast.warn("Basic Notification", {
+      position: "top-right",
+      autoClose: 5000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+    });
+    toast.error(CustomToast, {
+      position: "bottom-right",
+      autoClose: 5000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+    });
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <FaAccusoft color="purple" size="10rem" onClick={notify} />
     </div>
   );
 }
